b8ae6e88f9344955f9294a048d86e0dd810c70c3
Mirunalini <mirunalinirajan@gmail.com>
2019-11-14 16:21:42 +0530

    jwt_security

---
+++ src/main/java/com/spring/jwtSecurity/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,66 @@
+package com.spring.jwtSecurity.filters;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+	private AuthenticationManager authenticationManager;
+
+	public void JwtAtuhenticationFilter(AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
+
+		setFilterProcessesUrl(SecurityConstants.AUTH_LOGIN_URL);
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		Authentication authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+		return authenticationManager.authenticate(authenticationToken);
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authentication) throws IOException, ServletException {
+		User user = ((User) authentication.getPrincipal());
+
+		List<String> roles = ((Authentication) user).getAuthorities().stream().map(GrantedAuthority::getAuthority)
+				.collect(Collectors.toList());
+
+		byte[] signingKey = SecurityConstants.JWT_SECRET.getBytes();
+		String token = Jwts.builder().signWith(Keys.hmacShaKeyFor(signingKey), SignatureAlgorithm.HS512)
+				.setHeaderParam("typ", SecurityConstants.TOKEN_TYPE)
+				.setIssuer(SecurityConstants.TOKEN_ISSUER)
+				.setAudience(SecurityConstants.TOKEN_AUDIENCE)
+				.setSubject(user.getName())
+				.setExpiration(new Date(System.currentTimeMillis() + 864000000))
+				.claim("rol", roles)
+				.compact();
+
+		response.addHeader(SecurityConstants.TOKEN_HEADER, SecurityConstants.TOKEN_PREFIX + token);
+
+	}
+
+}
